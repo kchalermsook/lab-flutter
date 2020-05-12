e0b627b1f49f95eb7d27d9fb9e3ba209821dc266
Krissada Chalermsook <k.chalermsook@gmail.com>
2020-05-12 15:55:44 +0700

    Add transaction.dart sample code

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,67 @@
 import 'package:flutter/material.dart';
-import './question.dart';
+import './transaction.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  var _answerText = "";
-  var _isCorrect = false;
-  void _onPressTomHank() {
-    print('This is Tom Hank');
-
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-      _answerText = "True";
-      _isCorrect = true;
-    });
-    print('Index = $_questionIndex');
-  }
-
-  @override
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    var questions = ['Who is your daddy?', 'Who is your mom?'];
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Justin Bieber'),
-              onPressed: () {
-                print('I am clicking Justin Bieber');
-                setState(() {
-                  _answerText = "False";
-                  _isCorrect = false;
-                });
-              },
+    return MaterialApp(title: 'Flutter App', home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'Pay for Krapao Kai', amount: 200, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Buy the new notebook', amount: 20000, date: DateTime.now()),
+  ];
+  
+  Widget _buildCard(Transaction tx) => SizedBox(
+    height: 210,
+    child: Card(
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(tx.title,
+                style: TextStyle(fontWeight: FontWeight.w500)),
+            subtitle: Text(tx.id),
+            leading: Icon(
+              Icons.insert_emoticon,
+              color: Colors.blue[500],
             ),
-            RaisedButton(
-              child: Text('Bruce Willis '),
-              onPressed: () {
-                print('Bruce Willis is my dad');
-                setState(() {
-                  _answerText = "False";
-                  _isCorrect = false;
-                });
-              },
+          ),
+          Divider(),
+          ListTile(
+            title: Text(tx.amount.toString(),
+                style: TextStyle(fontWeight: FontWeight.w500)),
+            leading: Icon(
+              Icons.attach_money,
+              color: Colors.blue[500],
             ),
-            RaisedButton(
-              child: Text('Tom Hank'),
-              onPressed: _onPressTomHank,
+          ),
+          ListTile(
+            title: Text(tx.date.toString()),
+            leading: Icon(
+              Icons.today,
+              color: Colors.blue[500],
             ),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              child: RaisedButton(
-                child: Text('Nicolas Cage'),
-                onPressed: () {
-                  print("Click Nicolas Cage");
-                  setState(() {
-                    _answerText = "False";
-                    _isCorrect = false;
-                  });
-                },
-              ),
-            ),
-            _isCorrect ? Question("True") : Text("False")
-          ],
-        ),
+          ),
+        ],
+      ),
+    ),
+  );
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: transactions.map((tx){
+          return _buildCard(tx);
+        }).toList()
       ),
     );
   }
 }
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Text('ssss')
-//     );
-//   }
-// }
