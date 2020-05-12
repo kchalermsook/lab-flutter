fe23a93e0112288f3b573a938d0d917ddd93ddef
Krissada Chalermsook <k.chalermsook@gmail.com>
2020-05-12 13:40:33 +0700

    Import ALl

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'test.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  
+
+  void _onPressTomHank(){
+    print('This is Tom Hank');
+      
+    setState((){
+      _questionIndex = _questionIndex +1;
+    });
+    print('Index = $_questionIndex');
+    
+  }
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'Who is your daddy?',
+      'Who is your mom?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[_questionIndex]),
+            RaisedButton(
+              child: Text('Justin Bieber'),
+              onPressed: ()=>print('I am clicking Justin Bieber'),
+            ),
+            RaisedButton(
+              child: Text('Bruce Willis '),
+              onPressed: (){print('Bruce Willis is my dad');},
+            ),
+            RaisedButton(
+              child: Text('Tom Hank'),
+              onPressed: _onPressTomHank ,
+            ),
+            RaisedButton(
+              child: Text('Nicolas Cage'),
+              onPressed: ()=> print("Click Nicolas Cage"),
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Text('ssss')
+//     );
+//   }
+// }
---
+++ lib/test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First Appss'),
+        ),
+        body: Text('Hello Worlddd22'),
+      ),
+    );
+  }
+}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Text('ssss')
+//     );
+//   }
+// }
